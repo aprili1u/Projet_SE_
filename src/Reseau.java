83e1ed3b571a9a338d1e62c8723cdd9036a9d7bb
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-12-02 16:48:22 +0100

    Structure attributs et méthodes

--- src/Reseau.java
+++ src/Reseau.java
@@ -1,2 +1,52 @@
 public class Reseau {
+	private Paquet paquets[];
+	private Arc arcs[];
+	private Noeud noeuds[];
+	private int temps;
+	private int tauxActualisation;
+	
+	public Reseau() {
+		// Constructeur
+		Paquet paquets[] = new paquet[];
+		Arc arcs[] = new paquet[];
+		Paquet paquets[] = new paquet[];
+	}
+	
+	private void addPaquet() {
+		// Mise à jour de paquets[]
+	}
+	
+	private void addArc() {
+		// Mise à jour de arcs[]
+	}
+	
+	private void addNoeud() {
+		// Mise à jour de noeuds[]
+	}
+	
+	private void removePaquet() {
+		// Mise à jour de paquets[]
+	}
+	
+	private void removeArc() {
+		// Mise à jour de arcs[]
+	}
+	
+	private void removeNoeud() {
+		// Mise à jour de noeuds[]
+	}
+	
+	private void tic() {
+		// Écoulement du temps
+		this.temps++;
+	}
+	
+	private void deplacerPaquet(Paquet p, int path[]) {
+		// Mise à jour arcs[], noeuds[] et paquets[]
+	}
+	
+	private int[] plusCourtChemin(Paquet p) {
+		// Calcul du plus court chemin
+		return path;
+	}
 }
