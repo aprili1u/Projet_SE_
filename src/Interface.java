a2f3111fe9931622de8ab303f3b4f49202992cc6
aymanemoataz1 <aymanemoataz@depinfonancy.net>
2019-12-02 18:23:30 +0100

    Ajout de tests unitaires

--- src/Interface.java
+++ src/Interface.java
@@ -1,3 +1,2 @@
 public class Interface {
-    publ
 }
--- test/PriorityQTest.java
+++ test/PriorityQTest.java
@@ -1,28 +1,64 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PriorityQTest {
-
-    @org.junit.jupiter.api.Test
-    void insertElement() {
+    @Test
+    public void insertElementTest() {
+        PriorityQ F = new PriorityQ(50);
+        Paquet p = new Paquet(null,null,50,null);
+        F.insertElement(p);
+        assertEquals(F.size(), 1);
     }
 
-    @org.junit.jupiter.api.Test
-    void popElement() {
+
+    @Test
+    public void popElementTest() {
+        PriorityQ F = new PriorityQ(50);
+        Paquet p = new Paquet(null,null,50,null);
+        Paquet p1 = new Paquet(null,null,50,null);
+
+        F.insertElement(p);
+        F.insertElement(p1);
+
+        Integer s = F.size();
+        F.popElement();
+        assertEquals(F.size(), s - 1);
     }
 
-    @org.junit.jupiter.api.Test
-    void isEmpty() {
+    @Test
+    public void isEmptyTest() {
+        PriorityQ F = new PriorityQ(10);
+        Paquet p = new Paquet(null,null,50,null);
+        F.insertElement(p);
+        assertEquals(F.isEmpty(), false);
+        F.popElement();
+        assertEquals(F.isEmpty(), true);
     }
 
-    @org.junit.jupiter.api.Test
-    void size() {
+    @Test
+    public void sizeTest() {
+        PriorityQ F = new PriorityQ(10);
+        assertEquals(F.size(), 0);
+        Paquet p = new Paquet(null,null,50,null);
+        Paquet p1 = new Paquet(null,null,50,null);
+        F.insertElement(p);
+        F.insertElement(p1);
+        assertEquals(F.size(), 2);
+        F.popElement();
+        assertEquals(F.size(), 1);
+    }
+    @Test
+    public void toStringTest() {
+        PriorityQ F = new PriorityQ(3);
+        Paquet p = new Paquet(null,null,50,null);
+        Paquet p1 = new Paquet(null,null,50,null);
+        F.insertElement(p);
+        F.insertElement(p1);
+        assertEquals(F.toString(),"[5, 2, null]");
+
     }
 
-    @org.junit.jupiter.api.Test
-    void element() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void toString1() {
-    }
 }
