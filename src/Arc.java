731fb0685794b23d6bd18ea9f94da885464685f2
PaulAldeano <43879352+PaulAldeano@users.noreply.github.com>
2019-12-02 18:28:53 +0100

    Add files via upload
    Arc Class

---
+++ src/Arc.java
@@ -0,0 +1,79 @@
+public class Arc {
+    private Noeud depart;
+    private Noeud fin;
+    private int capacite;
+    private int longueur;
+    private ArrayList<Paquet> listePaquet;
+    private ArrayList<int> listePosition;
+
+    public Arc(Noeud depart, Noeud fin, int capacite, int longueur) {
+        this.depart = depart;
+        this.fin = fin;
+        this.capacite = 1;
+        this.longueur = longueur;
+        this.listePaquet=new ArrayList<Paquet>();
+        this.listePosition=new ArrayList<int>();
+    }
+
+    //Renvoie la capacité
+    public int getCapacite() {
+        return capacite;
+    }
+    //Renvoie le facteur
+    public int getLongueur() {
+        return longueur;
+    }
+    //Renvoie le noeud de départ
+    public Noeud getDepart() {
+        return depart;
+    }
+    //Renvoie le noeud de fin
+    public Noeud getFin() {
+        return fin;
+    }
+    //Met à jour la capacité
+    public void updateCapacite(int newCapacite){
+        this.capacite=newCapacite;
+    }
+    //Met à jour le facteur
+    public void updateLongueur(int newLongueur){
+        this.longueur=newLongueur;
+    }
+    //Vérifie si l'arc est plein
+    public boolean isFull(){
+        return listePaquet.size()<=capacite;
+    }
+    //vérifie si l'arc est vide
+    public boolean isEmpty() {
+        return listePaquet.isEmpty();
+    }
+    //Enlever un paquet
+    public Paquet removePaquet(int i) {
+        if (!this.isEmpty()) {
+            listePosition.remove(i);
+            return listePaquet.remove(i);
+        }
+    }
+    // retourne -1 s'il n'y a rien
+    public int refresh(){
+        for (int i=0,i<listePosition.size(),i++){
+            listePosition[i]++;
+            if (listePosition[i]==longueur){
+                return i;
+            }
+        }
+        return -1;
+    }
+    //ajoute le paquet en vérifiant si l'arc n'est pas plein
+    public boolean addPaquet(Paquet p){
+        if (!this.isFull()){
+            this.listePaquet.add(p);
+            this.listePosition.add(1);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
