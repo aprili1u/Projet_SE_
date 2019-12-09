6b1f8715ed95eed159399145ffceb68478e8d773
jaaakaaa <43885753+jaaakaaa@users.noreply.github.com>
2019-12-09 22:01:40 +0100

    Update PaquetTest.java

--- test/PaquetTest.java
+++ test/PaquetTest.java
@@ -1,4 +1,24 @@
-import static org.junit.Assert.*;
+import org.junit.*;
 
-class PaquetTest {
+import java.util.ArrayList;
+
+
+public class PaquetTest {
+
+    @Test
+    public void main() {
+        ArrayList s = new ArrayList(3);
+        s.add(new Noeud(1));s.add(new Noeud(5));s.add(new Noeud(3));
+        Noeud noeud_depart = new Noeud(1);
+        Noeud noeud_destination = new Noeud(3);
+        Paquet paquet = new Paquet(noeud_depart,noeud_destination,50,s);
+
+
+        Paquet paquet2 = new Paquet(noeud_depart,noeud_destination,50,s);
+
+        System.out.println(paquet.getId());
+        System.out.println(paquet2.getId());
+        Assert.assertEquals(paquet.getId(), 1);
+        Assert.assertEquals(paquet2.getId(), 2);
+    }
 }
