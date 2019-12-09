58a725d553ceaa5c53c4bee3ab855d412dbdc91b
mullerma1u <celian.muller-machi5@etu.univ-lorraine.fr>
2019-12-09 17:55:39 +0100

    Tests unitaires sur Noeud et PriorityQ terminés

--- test/NoeudTest.java
+++ test/NoeudTest.java
@@ -4,17 +4,37 @@ class NoeudTest {
 
     @org.junit.jupiter.api.Test
     void getID() {
+        Noeud node = new Noeud(7);
+        assertEquals(node.getID(),7);
     }
 
     @org.junit.jupiter.api.Test
     void getQueue() {
+        Noeud node = new Noeud(7);
+        assertTrue(node.getQueue().isEmpty());
     }
 
     @org.junit.jupiter.api.Test
     void enterPaquet() {
+        Noeud node = new Noeud(7);
+        Paquet pack = new Paquet(node, node, 5, null);
+        node.enterPaquet(pack);
+        assertFalse(node.getQueue().isEmpty());
+        assertEquals(node.getQueue().element(), pack);
     }
 
     @org.junit.jupiter.api.Test
     void exitPaquet() {
+        Noeud node = new Noeud(7);
+        Paquet pack = new Paquet(node,node,5,null);
+        Paquet pack2 = new Paquet(node,node,7,null);
+        Paquet pack3 = new Paquet(node,node,3,null);
+        node.enterPaquet(pack);
+        node.enterPaquet(pack2);
+        node.enterPaquet(pack3);
+        assertEquals(node.exitPaquet(),pack2);
+        assertEquals(node.exitPaquet(),pack);
+        assertEquals(node.exitPaquet(),pack3);
+        assertTrue(node.getQueue().isEmpty());
     }
 }
