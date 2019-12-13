96a83bb7634df07405b9344a73c425deb4f1e3c5
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-12-13 23:01:34 +0100

    Plus court chemin (encore bugs)

--- src/Reseau.java
+++ src/Reseau.java
@@ -26,6 +26,131 @@ public class Reseau {
 		return this.noeuds;
 	}
 
+	public void addPaquet(Paquet paquet) {
+		// Mise a jour de paquets[]
+		this.paquets.add(paquet);
+	}
+
+	public void addArc(Arc arc) {
+		// Mise a jour de arcs[]
+		this.arcs.add(arc);
+	}
+
+	public void addNoeud(Noeud noeud) {
+		// Mise a jour de noeuds[]
+		this.noeuds.add(noeud);
+	}
+
+	public void removePaquet(Paquet paquet) {
+		// Mise a jour de paquets[]
+		for(int i=0; i<paquets.size(); i++) {
+			if(paquets.get(i) == paquet) {
+				this.paquets.remove(i);
+				return;
+			}
+		}
+		return;
+	}
+
+	public void removeArc(Arc arc) {
+		// Mise a jour de arcs[]
+		for(int i=0; i<arcs.size(); i++) {
+			if(arcs.get(i) == arc) {
+				this.arcs.remove(i);
+				return;
+			}
+		}
+		return;
+	}
+
+	public void removeNoeud(Noeud noeud) {
+		// Mise a jour de noeuds[]
+		for(int i=0; i<noeuds.size(); i++) {
+			if(noeuds.get(i) == noeud) {
+				this.noeuds.remove(i);
+				return;
+			}
+		}
+		return;
+	}
+
+	public Arc getArcFromNodes(Noeud depart, Noeud fin) {
+		for(Arc a : this.arcs) {
+			if(a.getDepart() == depart && a.getFin() == fin) {
+				return a;
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Arc> getArcsFromNode(Noeud depart) {
+		ArrayList<Arc> relatedArcs = new ArrayList<Arc>();
+		for(Arc a : this.arcs) {
+			if(a.getDepart() == depart) {
+				relatedArcs.add(a);
+			}
+		}
+		return relatedArcs;
+	}
+
+	public ArrayList<Noeud> getNeighbours(Noeud node) {
+		ArrayList<Noeud> neighbours = new ArrayList<Noeud>();
+		for(Arc a : getArcsFromNode(node)) {
+			neighbours.add(a.getFin());
+		}
+		return neighbours;
+	}
+
+	public ArrayList<Noeud> plusCourtChemin(Paquet paquet) {
+		// Calcul du plus court chemin
+		int n = this.noeuds.size();
+		int weights[] = new int[n];
+		Noeud predecessors[] = new Noeud[n];
+		int adjacentMatrix[][] = arcToMatrix();
+		ArrayList<Noeud> visitedNodes = new ArrayList<Noeud>();
+		ArrayList<Noeud> path = new ArrayList<Noeud>();
+		Noeud dep = paquet.getNoeudDepart();
+		Noeud dest = paquet.getNoeudDestination();
+		for(Noeud node : this.noeuds) {
+			weights[node.getID()] = -1;
+		}
+		weights[dep.getID()] = 0;
+		for(Noeud node : getNeighbours(dep)) {
+			weights[node.getID()] = getArcFromNodes(dep, node).getLongueur();
+		}
+		int min;
+		Noeud minNode;
+		int minNodeWeight;
+		int currentNodeWeight;
+		int additionalWeight;
+		while(visitedNodes.size() < n) {
+			minNode = this.noeuds.get(0);
+			min = -1;
+			for (Noeud node : this.noeuds) {
+				if (!visitedNodes.contains(node) && (min == -1 || weights[node.getID()] < min)) {
+					minNode = node;
+					min = weights[node.getID()];
+				}
+			}
+			visitedNodes.add(minNode);
+			for (Noeud node : getNeighbours(minNode)) {
+				minNodeWeight = weights[minNode.getID()];
+				currentNodeWeight = weights[node.getID()];
+				additionalWeight = getArcFromNodes(minNode, node).getLongueur();
+				if (currentNodeWeight == -1 || currentNodeWeight > minNodeWeight + additionalWeight) {
+					weights[node.getID()] = minNodeWeight + additionalWeight;
+					predecessors[node.getID()] = minNode;
+				}
+			}
+		}
+		Noeud current = dest;
+		while(current != dep) {
+			path.add(current);
+			current = predecessors[current.getID()];
+		}
+		return path;
+	}
+
 	public int[][] arcToMatrix() {
 		// Construction de la matrice d'adjacence
 		int n = this.noeuds.size();
@@ -54,77 +179,21 @@ public class Reseau {
 				System.out.print("\n  ");
 			}
 		}
-		System.out.print("]");
-	}
-	
-	public void addPaquet(Paquet paquet) {
-		// Mise a jour de paquets[]
-		this.paquets.add(paquet);
-	}
-	
-	public void addArc(Arc arc) {
-		// Mise a jour de arcs[]
-		this.arcs.add(arc);
-	}
-	
-	public void addNoeud(Noeud noeud) {
-		// Mise a jour de noeuds[]
-		this.noeuds.add(noeud);
+		System.out.print("]\n");
 	}
 
-	public void removePaquet(Paquet paquet) {
-		// Mise a jour de paquets[]
-		for(int i=0; i<paquets.size(); i++) {
-			if(paquets.get(i) == paquet) {
-				this.paquets.remove(i);
-				return;
-			}
-		}
-		return;
-	}
-	
-	public void removeArc(Arc arc) {
-		// Mise a jour de arcs[]
-		for(int i=0; i<arcs.size(); i++) {
-			if(arcs.get(i) == arc) {
-				this.arcs.remove(i);
-				return;
-			}
-		}
-		return;
-	}
-	
-	public void removeNoeud(Noeud noeud) {
-		// Mise a jour de noeuds[]
-		for(int i=0; i<noeuds.size(); i++) {
-			if(noeuds.get(i) == noeud) {
-				this.noeuds.remove(i);
-				return;
-			}
-		}
-		return;
-	}
-	
-	public void tic() {
-		// Ecoulement du temps
-		this.temps++;
-	}
-	
 	public void deplacerPaquet(Paquet paquet, int path[]) {
 		// Mise a jour arcs[], noeuds[] et paquets[]
-		
 		// Probleme: comment metre a jour la position du paquet dans l'arc sans
 		// avoir acces a la taille de l'arc
-		
 		return;
 	}
-	
-	public int[] plusCourtChemin(Paquet paquet) {
-		// Calcul du plus court chemin
-		int n = this.arcs.size();
-		Noeud dep = paquet.getNoeudDepart();
-		Noeud dest = paquet.getNoeudDestination();
-		int nullMatrix[][] = new int[n][n];
-		return new int[2];
+
+	public void refresh() {
+		// Mise à jour générale
+		for(Arc a : this.arcs) {
+			a.refresh();
+		}
+		this.temps++;
 	}
 }
